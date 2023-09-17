cba12fd7b81b7e40c01aaa09745cbc0031e08f5f
unknown <sricharanmurukutla@gmail.com>
2023-09-17 09:03:48 -0600

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,58 +1,23 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const fileSystem = require('fs')
+const importJSON = fileSystem.readFileSync('budget.json', 'utf8');
+const budgetData = JSON.parse(importJSON);
 
-app.use('/', express.static('public'));
-
-const budget = {
-    "myBudget": [
-      {
-        "title": "Rent",
-        "budget": 375
-      },
-      {
-        "title": "Eat Out",
-        "budget": 25
-      },
-      {
-        "title": "Grocery",
-        "budget": 110
-      },
-      {
-        "title": "Healthcare",
-        "budget": 100
-      },
-      {
-        "title": "Clothing",
-        "budget": 55
-      },
-      {
-        "title": "Travel",
-        "budget": 40
-      },
-      {
-        "title": "Gas",
-        "budget": 80
-      },
-      {
-        "title": "Savings",
-        "budget": 90
-      },
-      {
-        "title": "Pet Expenses",
-        "budget": 70
-      }
-    ]
-  };
-
-app.get('/hello', (req, res) => {
-    res.send('Hello World!');
+app.get('/hello', (req, res) => 
+{
+res.send('Hello World!');
 });
 
-app.get('/budget',(req, res) => {
-    res.json(budget);
+app.listen(port, () =>
+{
+console.log(`Example app listening at http://localhost:${port}`);
+}
+);
+
+app.get('/budget', (req, res) => {
+    res.json(budgetData);
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-})
+app.use('/',express.static('public'));
